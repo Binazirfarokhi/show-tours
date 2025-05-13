fc9d0bfc6d92eb15ece12281d9dab39eb16fecc8
Binazir <binazir.farokhi@gmail.com>
2025-05-13 12:22:36 -0700

    create a before content

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,12 @@
-import chefClaudeLogo from "../images/chef-claude-icon.png"
+import chefClaudeLogo from "../images/chef-claude-icon.png";
 
-export default function Header(){
-    return(
-        <>
-        <header>
-             <img src={chefClaudeLogo} alt="chef-logo" />
+export default function Header() {
+  return (
+    <>
+      <header>
+        <img src={chefClaudeLogo} alt="chef-logo" />
         <h1>Chef Claude</h1>
-        </header>
-       
-        
-        </>
-    )
+      </header>
+    </>
+  );
 }
--- components/Main.jsx
+++ components/Main.jsx
@@ -1,13 +1,16 @@
-export default function Main(){
-    return(
-        <>
-        <main>
-            <form action="" className="add-ingredient-form">
-        <input type="text" aria-label="add ingredient"placeholder="e.g. egg"/>
-        <button>Add ingredient</button>
-     </form>
-        </main>
-     
-        </>
-    )
+export default function Main() {
+  return (
+    <>
+      <main>
+        <form action="" className="add-ingredient-form">
+          <input
+            type="text"
+            aria-label="add ingredient"
+            placeholder="e.g. egg"
+          />
+          <button>Add ingredient</button>
+        </form>
+      </main>
+    </>
+  );
 }

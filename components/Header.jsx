d19668944fb3066a40b2cfad9c410ee6d8f92169
Binazir <binazir.farokhi@gmail.com>
2025-05-13 07:39:01 -0700

    initial

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header(){
+    return(
+        <>
+        <header>
+             <img src="./images/chef-claude-icon.png" alt="chef-logo" />
+        <h1>Chef Claude</h1>
+        </header>
+       
+        
+        </>
+    )
+}

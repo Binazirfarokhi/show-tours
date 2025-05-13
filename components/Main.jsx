2aeea0f0890126145e92d3804b94cd94e92a97ff
Binazir <binazir.farokhi@gmail.com>
2025-05-13 12:19:14 -0700

    create button

---
+++ components/Main.jsx
@@ -0,0 +1,13 @@
+export default function Main(){
+    return(
+        <>
+        <main>
+            <form action="" className="add-ingredient-form">
+        <input type="text" aria-label="add ingredient"placeholder="e.g. egg"/>
+        <button>Add ingredient</button>
+     </form>
+        </main>
+     
+        </>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Header from '../components/Header'
+import Main from '../components/Main'
 
 export default function App(){
     return (
         <>
        
         <Header/>
+        <Main/>
+
         </>
     )
 }

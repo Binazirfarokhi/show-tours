8c201c74bfae2b4d94f3e5900eb39fc575784579
Binazir <binazir.farokhi@gmail.com>
2025-05-15 09:41:25 -0700

    more changes

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -41,24 +41,5 @@ function increment(){
     )
 }
 
-export default function App() {
-    /**
-     * Challenge: 
-     * - Initialize state for `isGoingOut` as a boolean
-     * - Make it so clicking the button flips that
-     *   boolean value (true -> false, false -> true)
-     * - Display "Yes" if `isGoingOut` is `true`, "No" otherwise
-     */
-    const [isGoingOut, setIsGoingOut] = React.useState('true')
-   function changeMind(){
-    setIsGoingOut(prev =>!prev)
-   }
 
-    return (
-        <main>
-            <h1 className="title">Do I feel like going out tonight?</h1>
-            <button className="value" onClick={changeMind}>{isGoingOut ? "yes" : "No"}</button>
-        </main>
-    )
-}
 

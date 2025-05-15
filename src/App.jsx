ec9ac6e665fdaca9fd3523b71adaf351da77b669
Binazir <binazir.farokhi@gmail.com>
2025-05-15 11:30:26 -0700

    polishing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-import Header from '../components/Header'
-import Main from '../components/Main'
-import React from "react"
-import Practice   from './Practice'
-export default function App(){
-    return (
-        <>
-       
-        <Header/>
-        <Main/>
-        <Practice />
+import Header from "./Header"
+import Main from "./Main"
 
-        </>
-    )
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  )
 }
-// props are immutable 
-// state are manage by somponenet itself
-// value which changes need state to make sure render is working for you

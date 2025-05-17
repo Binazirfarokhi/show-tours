b85c4a661449c8eb53ad993b2e936e771525782f
Binazir <binazir.farokhi@gmail.com>
2025-05-17 06:52:22 -0700

    react-setup

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,12 +1,25 @@
+import React from 'react'
+/**
+     * Challenge: Update our app so that when the user enters a
+     * new ingredient and submits the form, it adds that new
+     * ingredient to our list!
+     */
+
+
 export default function Main() {
     const ingredients =["chicken", "apple", "tomatos"]
+ const [count, setCount] = React.useState([])
+ const[newitem, setNewitem]= React.useState('')
+ function isAdd(){
+    setCount([...count,newitem])
+    setNewitem('');
+ }
+ 
+    // const showlist = ingredients.map(item=>(
 
-    const showlist = ingredients.map(item=>(
-
-       <li key={item}>{item}</li>
-    )
-
-    )
+    //    <li key={item}>{item}</li>
+    // )
+    // )
     function handleSubmit(event){
         event.preventDefault()
         
@@ -27,7 +40,7 @@ export default function Main() {
           <button>Add ingredient</button>
         </form>
         <ul>
-            {showlist}
+            {isAdd}
         </ul>
       </main>
     </>
